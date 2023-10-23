fb7c191fd11ed3324a22685dc30cc98538c40e2d
Liam Hess <liamskydamien@gmail.com>
2023-10-23 09:08:29 +0200

    add language menu

--- src/components/header/LifeChartHeader.js
+++ src/components/header/LifeChartHeader.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import {GrLanguage} from 'react-icons/gr';
 import "../../output.css";
+import {Languages} from "../../constants/languages";
 const LifeChartHeader = () => {
     return (
         <div className="navbar rounded-lg">
@@ -7,16 +9,20 @@ const LifeChartHeader = () => {
                 <a className="navbar-item">Ripple UI</a>
             </div>
             <div className="navbar-end">
-                <div className="avatar avatar-ring avatar-md">
+                <div>
                     <div className="dropdown-container">
                         <div className="dropdown">
                             <label className="btn btn-ghost flex cursor-pointer px-0" tabIndex="0">
-                                <img src="https://i.pravatar.cc/150?u=a042581f4e29026024d" alt="avatar" />
+                                <GrLanguage size="24"/>
                             </label>
                             <div className="dropdown-menu dropdown-menu-bottom-left">
-                                <a className="dropdown-item text-sm">Profile</a>
-                                <a tabIndex="-1" className="dropdown-item text-sm">Account settings</a>
-                                <a tabIndex="-1" className="dropdown-item text-sm">Subscriptions</a>
+                                {
+                                    Languages.map((language, index) => {
+                                        return (
+                                            <a key={index} className="dropdown-item text-sm">{language.label}</a>
+                                        );
+                                    })
+                                }
                             </div>
                         </div>
                     </div>
---
+++ src/constants/languages.js
@@ -0,0 +1,14 @@
+export const Languages = [
+    {
+        "label": "English",
+        "value": "en"
+    },
+    {
+        "label": "Tiếng Việt",
+        "value": "vi"
+    },
+    {
+        "label": "Deutsch",
+        "value": "de"
+    }
+];
