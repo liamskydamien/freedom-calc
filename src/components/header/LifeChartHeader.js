12658802366cef584d17f2e677d35c9a2f606463
Liam Hess <liamskydamien@gmail.com>
2023-10-23 20:13:15 +0200

    add darkmode toggle

--- src/components/header/LifeChartHeader.js
+++ src/components/header/LifeChartHeader.js
@@ -1,34 +1,39 @@
 import React, {useEffect, useState} from 'react';
-import {MdLanguage, MdOutlineNightsStay} from 'react-icons/md';
+import {MdLanguage, MdOutlineNightsStay, MdSunny} from 'react-icons/md';
 import "../../output.css";
 import {Languages} from "../../constants/languages";
 import i18n from "i18next";
+import useDarkmode from "../../hooks/useDarkmode";
 const LifeChartHeader = () => {
 
     const [language, setLanguage] = useState('en');
-
     useEffect(() => {
             i18n.changeLanguage(language).then(r => console.log('Language changed to ' + language));
     }, [language]);
 
+    const {theme, toggleTheme} = useDarkmode();
+
     return (
         <div className="navbar rounded-lg">
             <div className="navbar-start">
-                <a className="navbar-item">Ripple UI</a>
+                <a className="navbar-item">Freedom Calc</a>
             </div>
             <div className="navbar-end">
                 <div className="flex">
                     <div className="dropdown-container">
-                        <div className="dropdown" style={{marginRight: "1rem"}}>
+                        <div style={{marginRight: "1rem"}}>
                             <label className="btn btn-ghost flex cursor-pointer px-0" tabIndex="0">
-                                <a className="reactIcons">
-                                    <MdOutlineNightsStay size="32"/>
+                                <a className="reactIcons"
+                                    onClick={() => toggleTheme()}
+                                >
+                                    {
+                                        theme === 'light' ?
+                                                <MdOutlineNightsStay size="32"/>
+                                            :
+                                                <MdSunny size="32"/>
+                                    }
                                 </a>
                             </label>
-                            <div className="dropdown-menu dropdown-menu-bottom-left">
-                                <a className="dropdown-item text-sm">Dark mode</a>
-                                <a className="dropdown-item text-sm">Light mode</a>
-                            </div>
                         </div>
                     </div>
                     <div className="dropdown-container">
