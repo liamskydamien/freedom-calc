0a8a28e7936c072cc527df2bc710c6db686a5f1a
Liam Hess <liamskydamien@gmail.com>
2023-10-25 06:33:54 +0200

    add step increase and decrease

--- src/components/navigation/NavigationLifeChart.js
+++ src/components/navigation/NavigationLifeChart.js
@@ -1,12 +1,22 @@
 import Stepper from "./Stepper";
 import {useState} from "react";
+import {MdKeyboardArrowLeft, MdKeyboardArrowRight} from "react-icons/md";
 
 const NavigationLifeChart = () => {
     const [step, setStep] = useState(1);
     return (
-        <div>
+        <div className="flex">
+            <label className="btn btn-ghost flex cursor-pointer px-0">
+                <a className="reactIcons">
+                    <MdKeyboardArrowLeft size="32" onClick={() => setStep(step > 1 ? step - 1 : 1)}/>
+                </a>
+            </label>
             <Stepper active={step} />
-            <button onClick={() => setStep(step + 1)}>Next</button>
+            <label className="btn btn-ghost flex cursor-pointer px-0">
+                <a className="reactIcons">
+                    <MdKeyboardArrowRight size="32" onClick={() => setStep(step < 5? step + 1 : 5)}/>
+                </a>
+            </label>
         </div>
     )
 }
