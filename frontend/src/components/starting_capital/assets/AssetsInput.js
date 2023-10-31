3073c10af2e051f347985c18456c241873168752
Liam Hess <liamskydamien@gmail.com>
2023-10-31 12:51:39 +0100

    Add CryptoCurrencyInput.js

--- frontend/src/components/starting_capital/assets/AssetsInput.js
+++ frontend/src/components/starting_capital/assets/AssetsInput.js
@@ -2,6 +2,7 @@ import {useState} from "react";
 import LiquidAssetsInput from "./liquid_assets/LiquidAssetsInput";
 import RealEstateInput from "./realestate/RealEstateInput";
 import StockAssetInput from "./stocks/StockAssetInput";
+import CryptoCurrencyInput from "./crypto_currency/CryptoCurrencyInput";
 
 const AssetsInput = ({assets, t}) => {
     const [activeTab, setActiveTab] = useState(1);
@@ -31,11 +32,26 @@ const AssetsInput = ({assets, t}) => {
                 <label htmlFor="corporate" className="tab tab-pill">{t('stocks')}</label>
 
                 <input type="radio"
+                          id="crypto"
+                          name="crypto-tab"
+                          className="tab-toggle"
+                          onClick={() => setActiveTab(4)} />
+                <label htmlFor="crypto" className="tab tab-pill">{t('crypto_currency')}</label>
+
+                <input type="radio"
                        id="other-assets"
                        name="other-assets-tab"
                        className="tab-toggle"
-                       onClick={() => setActiveTab(4)} />
+                       onClick={() => setActiveTab(5)} />
                 <label htmlFor="other-assets" className="tab tab-pill">{t('other_assets')}</label>
+
+                <input type="radio"
+                       id="insurance"
+                       name="insurance-tab"
+                       className="tab-toggle"
+                       onClick={() => setActiveTab(6)} />
+                <label htmlFor="insurance" className="tab tab-pill">{t('insurance')}</label>
+
             </div>
             {
                 activeTab === 1 ?
@@ -51,7 +67,13 @@ const AssetsInput = ({assets, t}) => {
                     <StockAssetInput t={t}
                                      setStocks={assets.setStocks}/>
                     :
+                activeTab === 4 ?
+                    <CryptoCurrencyInput t={t} setCryptoCurrency={assets.setCryptoCurrency}/>
+                    :
+                activeTab === 5 ?
                     <h2>Not implemented yet</h2>
+                    :
+                    <></>
             }
         </>
     );
---
+++ frontend/src/components/starting_capital/assets/crypto_currency/CryptoCurrencyInput.js
@@ -0,0 +1,57 @@
+import {useEffect, useState} from "react";
+
+const CryptoCurrencyInput = ({t, setCryptoCurrency}) =>{
+    const [bitcoin, setBitcoin] = useState(0.0);
+    const [ethereum, setEthereum] = useState(0.0);
+    const [other, setOther] = useState(0.0);
+
+    useEffect(() => {
+        updateCryptoCurrency();
+    }, [bitcoin, ethereum, other]);
+    const updateCryptoCurrency = () => {
+        setCryptoCurrency(bitcoin + ethereum + other);
+    }
+
+    const bitcoinChangeHandler = (event) => {
+        setBitcoin(event.target.value);
+    }
+
+    const ethereumChangeHandler = (event) => {
+        setEthereum(event.target.value);
+    }
+
+    const otherChangeHandler = (event) => {
+        setOther(event.target.value);
+    }
+
+    return (
+        <div className="mx-auto flex w-full max-w-sm flex-col gap-6">
+            <div className="form-group">
+                <h3 className="text-sm font-bold pb-0">{t('crypto_currency')}</h3>
+                <div className="form-field">
+                    <label className="form-label">{t('bitcoin')}</label>
+                    <input value={bitcoin}
+                           type="number"
+                           className="input max-w-full"
+                           onChange={bitcoinChangeHandler}/>
+                </div>
+                <div className="form-field">
+                    <label className="form-label">{t('ethereum')}</label>
+                    <input value={ethereum}
+                           type="number"
+                           className="input max-w-full"
+                           onChange={ethereumChangeHandler}/>
+                </div>
+                <div className="form-field">
+                    <label className="form-label">{t('other')}</label>
+                    <input value={other}
+                           type="number"
+                           className="input max-w-full"
+                           onChange={otherChangeHandler}/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CryptoCurrencyInput;
