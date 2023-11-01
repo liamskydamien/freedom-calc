7c2815fc74409e1fd60df269313054a04077adab
Liam Hess <liamskydamien@gmail.com>
2023-11-01 07:05:47 +0100

    Styling

--- frontend/src/components/starting_capital/StartingCapitalInput.js
+++ frontend/src/components/starting_capital/StartingCapitalInput.js
@@ -9,7 +9,7 @@ const StartingCapitalInput = () => {
     const {assets, passiva} = useContext(InputContext);
     const [active, setActive] = useState(1);
     return (
-        <div className="flex-col card p-5 max-w-fit">
+        <div className="flex-col card p-5">
             <h1 className="text-xl font-bold">{t('starting_capital')}</h1>
             <div className="tabs">
                 <input type="radio"
@@ -18,14 +18,14 @@ const StartingCapitalInput = () => {
                        className="tab-toggle"
                        defaultChecked
                        onClick={() => setActive(1)} />
-                <label htmlFor="tab-4" className="tab tab-bordered px-6 pr-24 pl-24">{t('activa')}</label>
+                <label htmlFor="tab-4" className="tab tab-bordered px-6 pr-16 pl-16">{t('activa')}</label>
 
                 <input type="radio"
                        id="tab-5"
                        name="tab-2"
                        className="tab-toggle"
                        onClick={() => setActive(2)} />
-                <label htmlFor="tab-5" className="tab tab-bordered px-6 pr-24 pl-24">{t('passiva')}</label>
+                <label htmlFor="tab-5" className="tab tab-bordered px-6 pr-16 pl-16">{t('passiva')}</label>
             </div>
             {
                 active === 1 ?
