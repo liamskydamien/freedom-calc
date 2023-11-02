032b0625c6381aee222ae380e43efed8806a4019
Liam Hess <liamskydamien@gmail.com>
2023-11-02 05:45:47 +0100

    Add TypeScript Classes

--- frontend/src/models/LifePhase.ts
+++ frontend/src/models/LifePhase.ts
@@ -1,5 +1,20 @@
+import {Income} from "./Income";
+import {Expenses} from "./Expenses";
+
 export class LifePhase{
     fromAge: number;
     toAge: number;
-    inc
+    income: Income;
+    expenses: Expenses;
+
+    constructor(fromAge: number, toAge: number, income: Income, expenses: Expenses) {
+        this.fromAge = fromAge;
+        this.toAge = toAge;
+        this.income = income;
+        this.expenses = expenses;
+    }
+
+    calculateNetIncome(): number {
+        return this.income.getTotalIncome() - this.expenses.getTotalExpenses();
+    }
 }
