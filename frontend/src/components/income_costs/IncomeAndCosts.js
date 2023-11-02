f9296bf675801bcc016b28958b60835a4bf356ac
Liam Hess <liamskydamien@gmail.com>
2023-11-02 04:44:14 +0100

    Add component

---
+++ frontend/src/components/income_costs/IncomeAndCosts.js
@@ -0,0 +1,9 @@
+const IncomeAndCosts = () => {
+    return (
+        <div className="flex-col card p-5">
+            <h1 className="text-xl font-bold">{t('starting_capital')}</h1>
+        </div>
+    )
+}
+
+export default IncomeAndCosts;
