6055cccb1d19e4e1a5c51a80e9dd2ccdee7c5e40
Liam Hess <liamskydamien@gmail.com>
2023-11-06 08:41:45 +0100

    Interception

--- frontend/src/calculations/useCalculatePOF.ts
+++ frontend/src/calculations/useCalculatePOF.ts
@@ -3,40 +3,31 @@ type Point = {
     y: number;
 };
 
-export const useCalculatePOF = (wealth : number[], costs : number[], startAge : number) : Point | null => {
-    if (wealth.length < 2 || costs.length < 2) {
-        throw new Error('Each array must have at least two points (two years of data).');
+export const useCalculatePOF = (wealth : number[], costs : number[]) : Point | null => {
+    if (wealth.length !== costs.length) {
+        return null;
     }
+    let pof : Point | null = null;
 
-    // Calculate slope (m) and y-intercept (b) for each line
-    const getSlopeAndIntercept = (yValues: number[]): [number, number] => {
-        const x1 = 0;
-        const x2 = 1;
-        const y1 = yValues[0];
-        const y2 = yValues[1];
+    const wealthStart = wealth[0];
+    const costStart = costs[0];
 
-        const m = (y2 - y1) / (x2 - x1);
-        const b = y1 - m * x1;
-
-        return [m, b];
-    };
-
-    const [m1, b1] = getSlopeAndIntercept(wealth);
-    const [m2, b2] = getSlopeAndIntercept(costs);
-
-    // Check if lines are parallel (slopes are equal)
-    if (m1 === m2) {
-        // If the y-intercepts are also equal, the lines are coincident (the same line).
-        // Otherwise, they are parallel and never intersect.
-        return b1 === b2 ? null : null; // Adjust as needed to handle coincident lines.
+    if (wealthStart < costStart) {
+        for (let i = 0; i < wealth.length; i++) {
+            if (wealth[i] >= costs[i]) {
+                pof = {
+                    x: i,
+                    y: costs[i]
+                };
+                break;
+            }
+        }
     }
-
-    // Calculate x value of intersection point
-    const x = ((b2 - b1) / (m1 - m2) + startAge)
-
-    // Calculate y value of intersection point using one of the line equations
-    const y = m1 * x + b1;
-
-    // Return the intersection point
-    return { x, y };
+    else {
+        pof = {
+            x: 0,
+            y: 0
+        };
+    }
+    return pof;
 }
--- frontend/src/test/calculations/testUseCalculatePOF.test.ts
+++ frontend/src/test/calculations/testUseCalculatePOF.test.ts
@@ -3,27 +3,36 @@ import {useCalculatePOF} from "../../calculations/useCalculatePOF";
 /* Test the useCalculatePOF hook */
 describe('testing useCalculatePOF', () => {
     test('useCalculatePOF with POF in array (positive)', () => {
-        const wealth = [1000, 1200, 1400];
-        const costs = [800 , 1200, 1600];
-        const startAge = 20;
+        const wealth = [800, 1200, 1400];
+        const costs = [1600 , 1200, 800];
         const expectedResult = {
-            x: 21,
+            x: 1,
             y: 1200
         };
 
-        const result = useCalculatePOF(wealth, costs, startAge);
+        const result = useCalculatePOF(wealth, costs);
         expect(result).toEqual(expectedResult);
     });
     test('useCalculatePOF with POF between arrays (positive)', () => {
         const wealth = [1000, 1200, 1400];
-        const costs = [800 , 1100, 1500];
-        const startAge = 20;
+        const costs = [1200 , 1100, 1000];
         const expectedResult = {
-            x: 21.5,
+            x: 1,
             y: 1100
         };
 
-        const result = useCalculatePOF(wealth, costs, startAge);
+        const result = useCalculatePOF(wealth, costs);
+        expect(result).toEqual(expectedResult);
+    });
+    test('useCalculatePOF with POF in array (negative)', () => {
+        const wealth = [800, 1200, 1400];
+        const costs = [800 , 1200, 800];
+        const expectedResult = {
+            x: 0,
+            y: 0
+        };
+
+        const result = useCalculatePOF(wealth, costs);
         expect(result).toEqual(expectedResult);
     });
 })
