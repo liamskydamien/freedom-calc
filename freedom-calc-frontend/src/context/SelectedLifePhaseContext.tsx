51addf61202fbf364f53411dce8cedf4aa4c41ff
Liam Hess <liamskydamien@gmail.com>
2023-11-09 11:01:34 +0100

    Add Context

--- freedom-calc-frontend/src/context/SelectedLifePhaseContext.tsx
+++ freedom-calc-frontend/src/context/SelectedLifePhaseContext.tsx
@@ -1,4 +1,4 @@
-import {createContext, useState} from "react";
+import React, {createContext, useState} from "react";
 import {LifePhase} from "../models/lifephases/LifePhase";
 import {LifePhaseContextType} from "../models/types/LifePhaseContextTypes";
 
