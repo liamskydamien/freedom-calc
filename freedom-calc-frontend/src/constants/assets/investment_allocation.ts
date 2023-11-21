7d689d13280dd5e5c104ab18e648336b59b568df
Liam Hess <liamskydamien@gmail.com>
2023-11-21 04:12:44 +0100

    change calculations

--- freedom-calc-frontend/src/constants/assets/investment_allocation.ts
+++ freedom-calc-frontend/src/constants/assets/investment_allocation.ts
@@ -4,10 +4,10 @@ import { InvestmentWeights } from "../../models/pof/InvestmentWeights";
  * The investment allocation is the percentage of the total investment that is allocated to each asset group.
  */
 export const INVESTMENT_ALLOCATION: InvestmentWeights = new InvestmentWeights(
-  0.2,
-  0.2,
-  0.2,
-  0.2,
-  0.2,
+  0.1,
+  0.4,
+  0.1,
+  0.3,
+  0.1,
   0.0,
 );
