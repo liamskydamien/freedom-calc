703d9cb31b080eecafe0809b4485dd6c37842ec8
Liam Hess <liamskydamien@gmail.com>
2023-11-25 06:38:28 +0100

    Add Explanation.tsx component to write the documentation + how to guide later

--- freedom-calc-frontend/src/pages/PersonalInformation.tsx
+++ freedom-calc-frontend/src/pages/PersonalInformation.tsx
@@ -1,6 +1,7 @@
 import { useTranslation } from "react-i18next";
 import InputPersonalInformation from "../components/personal_information/InputPersonalInformation";
 import "./styles/personalinformation.css";
+import Explanation from "../components/personal_information/Explanation";
 const PersonalInformation = () => {
   const { t } = useTranslation();
 
@@ -8,7 +9,7 @@ const PersonalInformation = () => {
     <>
       <div className="flex flex-row gap-2">
         <InputPersonalInformation t={t} />
-        <div className="enterUserDetailsImage shadow-lg rounded max-w-full max-h-full" />
+        <Explanation t={t}/>
       </div>
     </>
   );
