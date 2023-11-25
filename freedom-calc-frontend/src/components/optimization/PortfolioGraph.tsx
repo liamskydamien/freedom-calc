ff8613701e643ed726205859c989c7d082876656
Liam Hess <liamskydamien@gmail.com>
2023-11-25 13:48:34 +0100

    Added graph component

--- freedom-calc-frontend/src/components/optimization/PortfolioGraph.tsx
+++ freedom-calc-frontend/src/components/optimization/PortfolioGraph.tsx
@@ -1,8 +1,54 @@
-const PortfolioGraph = () => {
+import {ComposedChart, Label, Legend, Line, ReferenceDot, ResponsiveContainer, Tooltip, XAxis, YAxis} from "recharts";
+import {COLORS} from "../../constants/colors/colors";
+import React from "react";
+
+type PortfolioGraphProps = {
+    t: any
+    graph: any
+    currency: string
+}
+const PortfolioGraph : React.FC<PortfolioGraphProps> = ({t, graph, currency}) => {
     return (
-        <div>
-            <h1>Portfolio Graph</h1>
-        </div>
+            <div className="card max-w-full p-5">
+                <h1>{t("life_line_title")}</h1>
+                <ResponsiveContainer width={800} height={400}>
+                    <ComposedChart data={graph}>
+                        <XAxis dataKey="name">
+                            <Label value={t("years")} offset={0} position="insideBottom" />
+                        </XAxis>
+                        <YAxis type="number">
+                            <Label
+                                value={t("in_1000000") + " " + currency}
+                                offset={100}
+                                angle={-90}
+                                position="insideLeft"
+                            />
+                        </YAxis>
+                        <Line
+                            dot={false}
+                            type="monotone"
+                            dataKey="wealth"
+                            stroke={
+                               COLORS.light.wealth
+                            }
+                            strokeWidth={3}
+                            connectNulls={true}
+                            name={t("wealth")}
+                        />
+                        <Line
+                            dot={false}
+                            type="monotone"
+                            dataKey="cost"
+                            stroke={COLORS.light.costs}
+                            strokeWidth={3}
+                            connectNulls={true}
+                            name={t("costs")}
+                        />
+                        <Tooltip />
+                        <Legend verticalAlign="top" height={36} />
+                    </ComposedChart>
+                </ResponsiveContainer>
+            </div>
     )
 }
 
