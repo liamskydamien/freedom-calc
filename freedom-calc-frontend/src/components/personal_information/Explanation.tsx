8bd88d1bffe3f999806515e54ab0ef5bcde0345f
Hai Nam <hainamwork23@gmail.com>
2023-12-10 11:50:42 +0700

    Add the explanation

--- freedom-calc-frontend/src/components/personal_information/Explanation.tsx
+++ freedom-calc-frontend/src/components/personal_information/Explanation.tsx
@@ -6,9 +6,8 @@ type ExplanationProps = {
 const Explanation : React.FC<ExplanationProps> = ({t}) => {
     return (
         <div className="card max-w-full p-5 overflow-auto">
-            <h1 className="text-xl font-bold">{t('explanation_title')}</h1>
-            <p>{t('explanation_text_1')}</p>
-            <p>{t('explanation_text_1')}</p>
+            <h1 className="text-4xl font-bold">{t('Life chart')}</h1>
+            <p className="text-left text-xl">{t('The life chart calculation project is used to develop methods to calculate the financial freedom point by using Markowitz`s Portfolio Theory. In this web application, the user can input their data(personal information, income) and their prediction so that when they go through five steps, they can know their predicted financial freedom point.')}</p>
         </div>
     )
 }
