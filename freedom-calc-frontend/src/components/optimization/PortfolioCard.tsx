4631bdccff320473fa2e149b1149256363d50fcb
Liam Hess <liamskydamien@gmail.com>
2023-12-10 08:45:22 +0100

    More styling changes

--- freedom-calc-frontend/src/components/optimization/PortfolioCard.tsx
+++ freedom-calc-frontend/src/components/optimization/PortfolioCard.tsx
@@ -21,18 +21,18 @@ const PortfolioCard : React.FC<PortfolioCardProps> = ({t}) => {
 
 
     return (
-        <div className="card p-5 flex-col justify-center max-w-fit">
+        <div className="card p-5 flex-col justify-center max-h-max">
             <h1 className="text-lg ">{t('portfolio')}</h1>
             <div className="tabs">
                 <input type="radio" id="tab-4" name="tab-2" className="tab-toggle" defaultChecked
                        onClick={() => handleTabChange(0)} />
-                <label htmlFor="tab-4" className="tab tab-bordered px-6">{t('your_portfolio')}</label>
+                <label htmlFor="tab-4" className="tab tab-bordered px-4">{t('your_portfolio')}</label>
 
                 <input type="radio" id="tab-5" name="tab-2" className="tab-toggle" onClick={() => handleTabChange(1)} />
-                <label htmlFor="tab-5" className="tab tab-bordered px-6">{t('secure_portfolio')}</label>
+                <label htmlFor="tab-5" className="tab tab-bordered px-4">{t('secure_portfolio')}</label>
 
                 <input type="radio" id="tab-6" name="tab-2" className="tab-toggle" onClick={() => handleTabChange(2)}/>
-                <label htmlFor="tab-6" className="tab tab-bordered px-6">{t('risky_portfolio')}</label>
+                <label htmlFor="tab-6" className="tab tab-bordered px-4">{t('risky_portfolio')}</label>
             </div>
             {
                 activeTab === 0 ?
