2d11402823dd647dcdf7d6a4aa7cdd0bad72769f
Liam Hess <liamskydamien@gmail.com>
2023-12-13 05:30:54 +0100

    Integrate Tab-Change

--- freedom-calc-frontend/src/components/optimization/PortfolioVisualization/PortfolioVisualization.tsx
+++ freedom-calc-frontend/src/components/optimization/PortfolioVisualization/PortfolioVisualization.tsx
@@ -1,25 +1,35 @@
 import {useTranslation} from "react-i18next";
 import PortfolioGraph from "../PortfolioGraph";
+import {useState} from "react";
+import RadarChartPortfolio from "./RadarChartPortfolio";
 type PortfolioVisualizationProps = {
     graph: any
     currency: string
 }
 const PortfolioVisualization : React.FC<PortfolioVisualizationProps> = ({graph, currency}) => {
     const { t } = useTranslation()
+    const [activeTab, setActiveTab] = useState(0);
     return (
         <div className="card max-w-full p-5">
             <h1 className="text-lg">{t("portfolio_visualization")}</h1>
             <div className="tabs">
-                <input type="radio" id="tab-9" name="tab-2" className="tab-toggle" defaultChecked/>
+                <input type="radio" id="tab-9" name="tab-2" className="tab-toggle" defaultChecked onClick={() => {setActiveTab(0)}}/>
                 <label htmlFor="tab-9" className="tab tab-bordered px-6">Tab 1</label>
 
-                <input type="radio" id="tab-10" name="tab-2" className="tab-toggle"/>
+                <input type="radio" id="tab-10" name="tab-2" className="tab-toggle" onClick={() => {setActiveTab(1)}}/>
                 <label htmlFor="tab-10" className="tab tab-bordered px-6">Tab 2</label>
 
-                <input type="radio" id="tab-10" name="tab-2" className="tab-toggle"/>
-                <label htmlFor="tab-10" className="tab tab-bordered px-6">Tab 3</label>
+                <input type="radio" id="tab-11" name="tab-2" className="tab-toggle" onClick={() => {setActiveTab(2)}}/>
+                <label htmlFor="tab-11" className="tab tab-bordered px-6">Tab 3</label>
             </div>
-            <PortfolioGraph t={t} graph={graph} currency={currency} />
+            {
+                activeTab === 0 ?
+                    <PortfolioGraph t={t} graph={graph} currency={currency} />
+                : activeTab === 1 ?
+                    <RadarChartPortfolio />
+                :
+                    <h1>Not created</h1>
+            }
         </div>
     )
 }
