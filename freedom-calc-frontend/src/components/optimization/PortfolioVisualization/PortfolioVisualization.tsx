55771673645db29d4239d2b8b28a1097eb56aed1
Liam Hess <liamskydamien@gmail.com>
2023-12-14 08:45:14 +0100

    Hide not needed button

--- freedom-calc-frontend/src/components/optimization/PortfolioVisualization/PortfolioVisualization.tsx
+++ freedom-calc-frontend/src/components/optimization/PortfolioVisualization/PortfolioVisualization.tsx
@@ -23,13 +23,10 @@ const PortfolioVisualization : React.FC<PortfolioVisualizationProps> = ({portfol
             <h1 className="text-lg">{t("portfolio_visualization")}</h1>
             <div className="tabs">
                 <input type="radio" id="tab-9" name="tab-2" className="tab-toggle" defaultChecked onClick={() => {setActiveTab(0)}}/>
-                <label htmlFor="tab-9" className="tab tab-bordered px-6">Tab 1</label>
+                <label htmlFor="tab-9" className="tab tab-bordered px-6">{t('pof_per_portfolio')}</label>
 
                 <input type="radio" id="tab-10" name="tab-2" className="tab-toggle" onClick={() => {setActiveTab(1)}}/>
-                <label htmlFor="tab-10" className="tab tab-bordered px-6">Tab 2</label>
-
-                <input type="radio" id="tab-11" name="tab-2" className="tab-toggle" onClick={() => {setActiveTab(2)}}/>
-                <label htmlFor="tab-11" className="tab tab-bordered px-6">Tab 3</label>
+                <label htmlFor="tab-10" className="tab tab-bordered px-6">{t('portfolio_comparison')}</label>
             </div>
             {
                 activeTab === 0 ?   <PortfolioGraph t={t} graph={graph} currency={currency} />
