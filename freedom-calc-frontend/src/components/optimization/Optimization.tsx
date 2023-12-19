18187d7bec446b9fcf4642369076e59ebe4cfdba
Liam Hess <liamskydamien@gmail.com>
2023-12-19 08:03:05 +0100

    Add loading state

--- freedom-calc-frontend/src/components/optimization/Optimization.tsx
+++ freedom-calc-frontend/src/components/optimization/Optimization.tsx
@@ -6,63 +6,66 @@ import RiskAssesment from "./RiskAssesment";
 import PortfolioCard from "./PortfolioCard";
 import PortfolioVisualization from "./PortfolioVisualization/PortfolioVisualization";
 import { SelectedStocksContext } from "../../context/SelectedStocksContext";
-import { PortfolioClass } from "../../models/optimization/PortfolioClass";
+import { PortfolioClass, Stock } from "../../models/optimization/PortfolioClass";
+import { InputContext } from "../../context/InputContext";
 
-const Optimization = () => {
-
-const { t } = useTranslation();
-const [modalIsOpen, setIsOpen] = useState(true);
-const [risk, setRisk] = useState(0);
-const {selectedStocks, notSelectedStocks} = useContext(SelectedStocksContext);
-const [selectIndex, setSelectIndex] = useState([] as string[]);
-const [personalPortfolioState, setPersonalPortfolio] = useState({} as PortfolioClass);
-const [safestPortfolioState, setSafestPortfolio] = useState({} as PortfolioClass);
-const [riskiestPortfolioState, setRiskiestPortfolio] = useState({} as PortfolioClass);
-
-useEffect(() => {
-  setSelectIndex(selectedStocks.map((stock) => stock[0]));
-}, [selectedStocks]);
-
-const {portfolio: personalPortfolio,
-  isLoading: personalLoading,
-  isError: personalError} = useFetchPortfolio(selectIndex, risk);
-
-const {portfolio: safestPortfolio,
-  isLoading: safestLoading,
-  isError: safestError} = useFetchPortfolio(selectIndex, 0);
-
-const {portfolio: riskiestPortfolio,
-  isLoading: riskiestLoading,
-  isError: riskiestError} = useFetchPortfolio(selectIndex, 100);
-
-const closeModal = (risk: number) => {
-  setRisk(risk);
-  setIsOpen(false);
-  console.log(risk);
+type OptimizationProps = {
+  risk: number
 }
 
-const stocksTest : string[] = [ "AMD", "UPS", "DB", "DIS", "VOW3", "INTC", "V", "AFX", "NVDA" ];
-const target_std_dev = 100;
+const Optimization : React.FC<OptimizationProps> = ({risk}) => {
+  const { t } = useTranslation();
+  const {personalInformation} = useContext(InputContext)
 
-const portfolios = [
-  personalPortfolio,
-  safestPortfolio,
-  riskiestPortfolio
-];
+  const stocksTest : string[] = [ "AMD", "UPS", "DB", "DIS", "VOW3", "INTC", "V", "AFX", "NVDA"];
+  const target_std_dev = 100;
 
-const income = [0,2,5,7,10,12,15,20,23,28,32,35,40,42,0,0,0,0,0,0,0,0,0,0,0,0]
-const costs = [100, 98, 95, 93, 90, 88, 85, 80, 77, 72, 68, 65, 60, 58, 55, 52, 48, 43, 40, 38, 34, 30, 27, 24, 20, 15]
+  const {portfolio, isLoading, isError} = useFetchPortfolio(stocksTest, target_std_dev);
 
-//const {portfolioChart, safestPOF, riskiestPOF, personalPOF} = createPortfolioChartAndPOF(portfolios, income, costs)
+  const stocks : Stock[] = [
+    // TODO delete this
+    new Stock("AAPL", 0.5, 0.1, 0.1),
+    new Stock("MSFT", 0.5, 0.1, 0.1),
+    new Stock("GOOG", 0.5, 0.1, 0.1),
+    new Stock("AMZN", 0.5, 0.1, 0.1),
+  ];
+
+  const stocks2 : Stock[] = [
+    // TODO delete this
+    new Stock("AAPL", 0.5, 0.1, 0.1),
+    new Stock("MSFT", 0.5, 0.1, 0.1),
+    new Stock("GOOG", 0.5, 0.1, 0.1),
+    new Stock("AMZN", 0.5, 0.1, 0.0),
+  ];
+
+  const portfolios = [
+    // TODO calculate portfolios
+    new PortfolioClass( 0.0425, 0.03, stocks),
+    new PortfolioClass(0.03, 0.01, stocks),
+    new PortfolioClass( 0.08, 0.04, stocks2)
+  ];
+
+  const income = [0,2,5,7,10,12,15,20,23,28,32,35,40,42,0,0,0,0,0,0,0,0,0,0,0,0]
+  const costs = [100, 98, 95, 93, 90, 88, 85, 80, 77, 72, 68, 65, 60, 58, 55, 52, 48, 43, 40, 38, 34, 30, 27, 24, 20, 15]
 
 
-return (
+  useEffect(() => {
+
+  }, [portfolio]);
+  const {portfolioChart, safestPOF, riskiestPOF, personalPOF} = createPortfolioChartAndPOF(portfolios, income, costs)
+
+  return (
   <div>
-      {
-        modalIsOpen && <RiskAssesment t={t} closeModal={closeModal}/>
-      }
-      <div className="flex flex-row gap-2">
-      </div>
+    {
+      isLoading ? <div className="spinner-circle max-w-2xl max-h-2xl ml-auto mr-auto mt-40 mb-40"></div>
+        : isError ? <div>Error</div>
+          : portfolio ?
+            <div className="flex flex-row gap-2">
+              <PortfolioCard t={t} portfolios={portfolio} currency={personalInformation.currency} />
+              <PortfolioVisualization portfolios={portfolio} graph={portfolioChart} currency={personalInformation.currency} />
+            </div>
+            : <div>Portfolio is undefined</div>
+    }
   </div>
 )
 }
--- freedom-calc-frontend/src/pages/OptimizationPage.tsx
+++ freedom-calc-frontend/src/pages/OptimizationPage.tsx
@@ -1,79 +1,28 @@
-import {useTranslation} from "react-i18next";
-import { useContext, useEffect, useState } from "react";
-import {PortfolioClass, Stock} from "../models/optimization/PortfolioClass";
-import {createPortfolioChartAndPOF} from "../calculations/graphs/createPortfolioChart";
+import { useState } from "react";
 import SelectedStocksProvider from "../context/SelectedStocksContext";
-import PortfolioCard from "../components/optimization/PortfolioCard";
-import PortfolioVisualization from "../components/optimization/PortfolioVisualization/PortfolioVisualization";
-import { useFetchPortfolio } from "../hooks/useFetchPortfolio";
-import { InputContext } from "../context/InputContext";
-
+import RiskAssesment from "../components/optimization/RiskAssesment";
+import { useTranslation } from "react-i18next";
+import Optimization from "../components/optimization/Optimization";
 const OptimizationPage = () => {
-
     const { t } = useTranslation();
     const [modalIsOpen, setIsOpen] = useState(true);
     const [risk, setRisk] = useState(0);
-    const {personalInformation} = useContext(InputContext)
-
     const closeModal = (risk: number) => {
         setRisk(risk);
         setIsOpen(false);
-        console.log(risk);
     }
-
-    const stocksTest : string[] = [ "AMD", "UPS", "DB", "DIS", "VOW3", "INTC", "V", "AFX", "NVDA"];
-    const target_std_dev = 100;
-
-    const {portfolio, isLoading, isError} = useFetchPortfolio(stocksTest, target_std_dev);
-
-    const stocks : Stock[] = [
-        // TODO delete this
-        new Stock("AAPL", 0.5, 0.1, 0.1),
-        new Stock("MSFT", 0.5, 0.1, 0.1),
-        new Stock("GOOG", 0.5, 0.1, 0.1),
-        new Stock("AMZN", 0.5, 0.1, 0.1),
-        ];
-
-    const stocks2 : Stock[] = [
-        // TODO delete this
-        new Stock("AAPL", 0.5, 0.1, 0.1),
-        new Stock("MSFT", 0.5, 0.1, 0.1),
-        new Stock("GOOG", 0.5, 0.1, 0.1),
-        new Stock("AMZN", 0.5, 0.1, 0.0),
-    ];
-
-    const portfolios = [
-        // TODO calculate portfolios
-        new PortfolioClass( 0.0425, 0.03, stocks),
-        new PortfolioClass(0.03, 0.01, stocks),
-        new PortfolioClass( 0.08, 0.04, stocks2)
-        ];
-
-    const income = [0,2,5,7,10,12,15,20,23,28,32,35,40,42,0,0,0,0,0,0,0,0,0,0,0,0]
-    const costs = [100, 98, 95, 93, 90, 88, 85, 80, 77, 72, 68, 65, 60, 58, 55, 52, 48, 43, 40, 38, 34, 30, 27, 24, 20, 15]
-
-
-    useEffect(() => {
-
-    }, [portfolio]);
-    const {portfolioChart, safestPOF, riskiestPOF, personalPOF} = createPortfolioChartAndPOF(portfolios, income, costs)
-
 
     return (
         <div>
             <SelectedStocksProvider>
                 {
-                    // modalIsOpen && <RiskAssesment t={t} closeModal={closeModal}/>
+                    modalIsOpen && <RiskAssesment t={t} closeModal={closeModal}/>
                 }
                 {
-                isLoading ? <div>Loading...</div>
-                  : isError ? <div>Error</div>
-                  : portfolio ?
-                <div className="flex flex-row gap-2">
-                    <PortfolioCard t={t} portfolios={portfolio} currency={personalInformation.currency} />
-                    <PortfolioVisualization portfolios={portfolio} graph={portfolioChart} currency={personalInformation.currency} />
-                </div>
-                    : <div>Portfolio is undefined</div>
+                    !modalIsOpen ?
+                      <Optimization risk={risk} />
+                      :
+                      <div className="spinner-circle max-w-2xl max-h-2xl ml-auto mr-auto mt-40 mb-40"></div>
                 }
             </SelectedStocksProvider>
         </div>
